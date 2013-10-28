2000fa3bf3314e9ff8f9111524dba587bd3ce4cc
Carl-Emil Kjellstrand <erbsman@gmail.com>
2013-10-28 13:03:49 +0100

    added some better data to the layout.

--- src/se/kjellstrand/viewholderantipattern/ItemAdapter.java
+++ src/se/kjellstrand/viewholderantipattern/ItemAdapter.java
@@ -8,31 +8,26 @@ import android.widget.BaseAdapter;
 import android.widget.TextView;
 
 /**
- * Created by erbsman on 7/25/13.
+ * Created by Carl-Emil Kjellstrand on 10/25/13.
  */
 public class ItemAdapter extends BaseAdapter {
 
     private final Context mContext;
 
-    private String data[];
+    private int count = 256;
 
     public ItemAdapter(Context context) {
         mContext = context;
-        data = mContext.getResources().getStringArray(R.array.string_array);
     }
 
     @Override
     public int getCount() {
-        return data.length;
+        return count;
     }
 
     @Override
     public Object getItem(int i) {
-        if (i > getCount()) {
-            return data[i];
-        } else {
-            return null;
-        }
+        return i;
     }
 
     @Override
@@ -42,21 +37,42 @@ public class ItemAdapter extends BaseAdapter {
 
     @Override
     public View getView(int i, View convertView, ViewGroup viewGroup) {
+
         if (convertView == null) {
             convertView = LayoutInflater.from(mContext).inflate(R.layout.list_detail, viewGroup, false);
         }
 
+        int digit = i;
+
         Holder h = Holder.get(convertView);
-        h.title.setText(data[i]);
+        String digit_text = mContext.getResources().getString(R.string.list_detail_digit);
+        h.digit.setText(String.format(digit_text, digit));
+
+        String digit_even_text = mContext.getResources().getString(R.string.list_detail_digit_even);
+        String digit_even = digit % 2 == 0 ? "true" : "false";
+        h.digit_even.setText(String.format(digit_even_text, digit_even));
+
+        String digit_primt_text = mContext.getResources().getString(R.string.list_detail_digit_prime);
+        h.digit_prime.setText(String.format(digit_primt_text, !new String(new char[digit]).matches(".?|(..+?)\\1+")));
+
+        String digit_hex_text = mContext.getResources().getString(R.string.list_detail_digit_hex);
+        h.digit_hex.setText(String.format(digit_hex_text, Integer.toHexString(digit)));
 
         return convertView;
     }
 
     private static final class Holder {
-        public TextView title;
+        public TextView digit;
+        public TextView digit_even;
+        public TextView digit_prime;
+        public TextView digit_hex;
 
         private Holder(View v) {
-            title = (TextView) v.findViewById(R.id.list_detail_title);
+            digit = (TextView) v.findViewById(R.id.list_detail_digit);
+            digit_even = (TextView) v.findViewById(R.id.list_detail_digit_even);
+            digit_prime = (TextView) v.findViewById(R.id.list_detail_digit_prime);
+            digit_hex = (TextView) v.findViewById(R.id.list_detail_digit_hex);
+
             v.setTag(this);
         }
 
