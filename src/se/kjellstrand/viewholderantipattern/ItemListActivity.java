75ad70248465ac9abbb1df4f1bd0983e73c287d7
Carl-Emil Kjellstrand <erbsman@gmail.com>
2013-10-30 20:11:38 +0100

    formating.

--- src/se/kjellstrand/viewholderantipattern/ItemListActivity.java
+++ src/se/kjellstrand/viewholderantipattern/ItemListActivity.java
@@ -3,7 +3,6 @@ package se.kjellstrand.viewholderantipattern;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 
-
 /**
  * Created by Carl-Emil Kjellstrand on 10/25/13.
  */
@@ -13,7 +12,6 @@ public class ItemListActivity extends FragmentActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_item_list);
-
     }
 
 }
