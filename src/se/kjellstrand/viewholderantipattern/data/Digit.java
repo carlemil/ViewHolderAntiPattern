725323890f064402ea847c1e5a95461e07570d08
Carl-Emil Kjellstrand <erbsman@gmail.com>
2013-11-04 15:42:03 +0100

    added a data object for handling "digits".

---
+++ src/se/kjellstrand/viewholderantipattern/data/Digit.java
@@ -0,0 +1,42 @@
+
+package se.kjellstrand.viewholderantipattern.data;
+
+public class Digit {
+    private int mDigit;
+
+    public int getDigit() {
+        return mDigit;
+    }
+
+    public void setDigit(int mDigit) {
+        this.mDigit = mDigit;
+    }
+
+    public boolean isEven() {
+        return mDigit % 2 == 0 ? true : false;
+    }
+
+    public boolean isPrime(){
+        return !new String(new char[mDigit]).matches(".?|(..+?)\\1+");
+    }
+
+    public boolean isFibonacci(){
+        return ((Math.sqrt(5 * Math.pow((mDigit), 2) + 4) -
+                Math.round(Math.sqrt(5 * Math.pow((mDigit), 2) + 4)) == 0) ||
+                (Math.sqrt(5 * Math.pow((mDigit), 2) - 4) -
+                        Math.round(Math.sqrt(5 * Math.pow((mDigit), 2) - 4)) == 0));
+    }
+
+    public String toString() {
+        return Integer.toString(mDigit);
+    }
+
+    public String toHexString() {
+        return Integer.toHexString(mDigit);
+    }
+
+    public String toOctalString() {
+        return Integer.toOctalString(mDigit);
+    }
+
+}
